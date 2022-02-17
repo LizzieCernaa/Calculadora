a497e018c52a988569ead742fd7c3ea49164b3d9
LizzieCernaa <98780118+LizzieCernaa@users.noreply.github.com>
2022-02-17 11:16:49 -0600

    Ingresa opciones

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa para calculadora");
+            Console.WriteLine("----------MENU----------");
+            Console.WriteLine("1. Sumar");
+            Console.WriteLine("2. Restar ");
+            Console.WriteLine("3. Multiplicar ");
+            Console.WriteLine("4. Dividir");
+            Console.WriteLine("Seleccione una opción");
+
+
+            Console. ReadKey();
+        }
+    }
+}
