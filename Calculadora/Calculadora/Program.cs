a22b91658f3d82f59c210aca2c966c990dec897b
LizzieCernaa <98780118+LizzieCernaa@users.noreply.github.com>
2022-02-21 17:09:50 -0600

    quitanto errores

--- Calculadora/Calculadora/Program.cs
+++ Calculadora/Calculadora/Program.cs
@@ -11,8 +11,10 @@ namespace Calculadora
         static void Main(string[] args)
 
         {
-            int A = 0;
-           
+            int a = 0;
+            double b = 0;
+            double c = 0;
+
             Console.WriteLine("Programa para calculadora");
             Console.WriteLine("----------MENU----------");
             Console.WriteLine("1 Sumar");
@@ -20,56 +22,56 @@ namespace Calculadora
             Console.WriteLine("3 Multiplicar ");
             Console.WriteLine("4. Dividir");
             Console.WriteLine("Seleccione una opción");
-            A = int.Parse(Console.ReadLine());
+            a = int.Parse(Console.ReadLine());
 
-            if (A == 1)
+            if (a == 1)
             {
                 Console.WriteLine("Ingrese el numero 1:");
-                double B = double.Parse(Console.ReadLine());
+                b = double.Parse(Console.ReadLine());
 
                 Console.WriteLine("Ingrese el numero 2:");
                 double C = double.Parse(Console.ReadLine());
 
-                double D = B + C;
+                double D = b + C;
                 Console.WriteLine("El resultado de la suma es:" + D);
             }
-           else if (A == 2)
+           else if (a == 2)
             {
                     Console.WriteLine("Ingrese el numero 1:");
-                    double B = double.Parse(Console.ReadLine());
+                    b = double.Parse(Console.ReadLine());
 
                     Console.WriteLine("Ingrese el numero 2:");
                     double C = double.Parse(Console.ReadLine());
 
-                    double D = B - C;
+                    double D = b - C;
                     Console.WriteLine("El resultado de la resta es:" + D);
                
             }
-            if (A == 3)
+            if (a == 3)
             {
                 Console.WriteLine("Ingrese el numero 1:");
-                double B = double.Parse(Console.ReadLine());
+                b = double.Parse(Console.ReadLine());
 
                 Console.WriteLine("Ingrese el numero 2:");
                 double C = double.Parse(Console.ReadLine());
 
-                double D = B * C;
+                double D = b * C;
                 Console.WriteLine("El resultado de la multiplicacion es:" + D);
 
             }
-            else if (A == 4)
+            else if (a == 4)
             {
                 Console.WriteLine("Ingrese el numero 1:");
-                double B = double.Parse(Console.ReadLine());
+                b = double.Parse(Console.ReadLine());
 
                 Console.WriteLine("Ingrese el numero 2:");
                 double C = double.Parse(Console.ReadLine());
 
-                double D = B / C;
+                double D = b / C;
                 Console.WriteLine("El resultado de la division es:" + D);
 
             }
-            else if (A == 5) 
+            else if (a == 5) 
             {
                 Console.WriteLine ("No es posible realizar ningun procedimien");   
             }
